d61a10fffd9d7ec359ec384178daa10aa71d0037
Yazan Turk <96770991+yazanturk@users.noreply.github.com>
2024-02-27 21:44:35 +0300

    advertisement

---
+++ lib/future/myads/data/model/my_ads_model.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'my_ads_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+MyAdsModel _$MyAdsModelFromJson(Map<String, dynamic> json) => MyAdsModel(
+      json['name'] as String?,
+      json['email'] as String?,
+    );
+
+Map<String, dynamic> _$MyAdsModelToJson(MyAdsModel instance) =>
+    <String, dynamic>{
+      'name': instance.name,
+      'email': instance.email,
+    };
