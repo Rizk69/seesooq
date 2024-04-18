dc2acd3b31e3122b40ac6e060aa861376eca8036
Rizk <https://github.com/Rizk69>
2024-04-18 22:07:30 +0200

    add frequently

---
+++ lib/future/setting/presentation/cubit/about_us_cubit/about_us_state.freezed.dart
@@ -0,0 +1,135 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'about_us_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$AboutUsState {
+  AboutUsStatus get aboutUsStatus => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $AboutUsStateCopyWith<AboutUsState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $AboutUsStateCopyWith<$Res> {
+  factory $AboutUsStateCopyWith(
+          AboutUsState value, $Res Function(AboutUsState) then) =
+      _$AboutUsStateCopyWithImpl<$Res, AboutUsState>;
+  @useResult
+  $Res call({AboutUsStatus aboutUsStatus});
+}
+
+/// @nodoc
+class _$AboutUsStateCopyWithImpl<$Res, $Val extends AboutUsState>
+    implements $AboutUsStateCopyWith<$Res> {
+  _$AboutUsStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? aboutUsStatus = null,
+  }) {
+    return _then(_value.copyWith(
+      aboutUsStatus: null == aboutUsStatus
+          ? _value.aboutUsStatus
+          : aboutUsStatus // ignore: cast_nullable_to_non_nullable
+              as AboutUsStatus,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$AboutUsStateImplCopyWith<$Res>
+    implements $AboutUsStateCopyWith<$Res> {
+  factory _$$AboutUsStateImplCopyWith(
+          _$AboutUsStateImpl value, $Res Function(_$AboutUsStateImpl) then) =
+      __$$AboutUsStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({AboutUsStatus aboutUsStatus});
+}
+
+/// @nodoc
+class __$$AboutUsStateImplCopyWithImpl<$Res>
+    extends _$AboutUsStateCopyWithImpl<$Res, _$AboutUsStateImpl>
+    implements _$$AboutUsStateImplCopyWith<$Res> {
+  __$$AboutUsStateImplCopyWithImpl(
+      _$AboutUsStateImpl _value, $Res Function(_$AboutUsStateImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? aboutUsStatus = null,
+  }) {
+    return _then(_$AboutUsStateImpl(
+      aboutUsStatus: null == aboutUsStatus
+          ? _value.aboutUsStatus
+          : aboutUsStatus // ignore: cast_nullable_to_non_nullable
+              as AboutUsStatus,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$AboutUsStateImpl implements _AboutUsState {
+  const _$AboutUsStateImpl({this.aboutUsStatus = AboutUsStatus.initial});
+
+  @override
+  @JsonKey()
+  final AboutUsStatus aboutUsStatus;
+
+  @override
+  String toString() {
+    return 'AboutUsState(aboutUsStatus: $aboutUsStatus)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$AboutUsStateImpl &&
+            (identical(other.aboutUsStatus, aboutUsStatus) ||
+                other.aboutUsStatus == aboutUsStatus));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, aboutUsStatus);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$AboutUsStateImplCopyWith<_$AboutUsStateImpl> get copyWith =>
+      __$$AboutUsStateImplCopyWithImpl<_$AboutUsStateImpl>(this, _$identity);
+}
+
+abstract class _AboutUsState implements AboutUsState {
+  const factory _AboutUsState({final AboutUsStatus aboutUsStatus}) =
+      _$AboutUsStateImpl;
+
+  @override
+  AboutUsStatus get aboutUsStatus;
+  @override
+  @JsonKey(ignore: true)
+  _$$AboutUsStateImplCopyWith<_$AboutUsStateImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
---
+++ lib/future/setting/presentation/cubit/terms_of_use_cubit/terms_of_use_state.freezed.dart
@@ -0,0 +1,137 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'terms_of_use_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$TermsOfUseState {
+  TermsOfUseStatus get termsOfUseStatus => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $TermsOfUseStateCopyWith<TermsOfUseState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $TermsOfUseStateCopyWith<$Res> {
+  factory $TermsOfUseStateCopyWith(
+          TermsOfUseState value, $Res Function(TermsOfUseState) then) =
+      _$TermsOfUseStateCopyWithImpl<$Res, TermsOfUseState>;
+  @useResult
+  $Res call({TermsOfUseStatus termsOfUseStatus});
+}
+
+/// @nodoc
+class _$TermsOfUseStateCopyWithImpl<$Res, $Val extends TermsOfUseState>
+    implements $TermsOfUseStateCopyWith<$Res> {
+  _$TermsOfUseStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? termsOfUseStatus = null,
+  }) {
+    return _then(_value.copyWith(
+      termsOfUseStatus: null == termsOfUseStatus
+          ? _value.termsOfUseStatus
+          : termsOfUseStatus // ignore: cast_nullable_to_non_nullable
+              as TermsOfUseStatus,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$TermsOfUseStateImplCopyWith<$Res>
+    implements $TermsOfUseStateCopyWith<$Res> {
+  factory _$$TermsOfUseStateImplCopyWith(_$TermsOfUseStateImpl value,
+          $Res Function(_$TermsOfUseStateImpl) then) =
+      __$$TermsOfUseStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({TermsOfUseStatus termsOfUseStatus});
+}
+
+/// @nodoc
+class __$$TermsOfUseStateImplCopyWithImpl<$Res>
+    extends _$TermsOfUseStateCopyWithImpl<$Res, _$TermsOfUseStateImpl>
+    implements _$$TermsOfUseStateImplCopyWith<$Res> {
+  __$$TermsOfUseStateImplCopyWithImpl(
+      _$TermsOfUseStateImpl _value, $Res Function(_$TermsOfUseStateImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? termsOfUseStatus = null,
+  }) {
+    return _then(_$TermsOfUseStateImpl(
+      termsOfUseStatus: null == termsOfUseStatus
+          ? _value.termsOfUseStatus
+          : termsOfUseStatus // ignore: cast_nullable_to_non_nullable
+              as TermsOfUseStatus,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$TermsOfUseStateImpl implements _TermsOfUseState {
+  const _$TermsOfUseStateImpl(
+      {this.termsOfUseStatus = TermsOfUseStatus.initial});
+
+  @override
+  @JsonKey()
+  final TermsOfUseStatus termsOfUseStatus;
+
+  @override
+  String toString() {
+    return 'TermsOfUseState(termsOfUseStatus: $termsOfUseStatus)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$TermsOfUseStateImpl &&
+            (identical(other.termsOfUseStatus, termsOfUseStatus) ||
+                other.termsOfUseStatus == termsOfUseStatus));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, termsOfUseStatus);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$TermsOfUseStateImplCopyWith<_$TermsOfUseStateImpl> get copyWith =>
+      __$$TermsOfUseStateImplCopyWithImpl<_$TermsOfUseStateImpl>(
+          this, _$identity);
+}
+
+abstract class _TermsOfUseState implements TermsOfUseState {
+  const factory _TermsOfUseState({final TermsOfUseStatus termsOfUseStatus}) =
+      _$TermsOfUseStateImpl;
+
+  @override
+  TermsOfUseStatus get termsOfUseStatus;
+  @override
+  @JsonKey(ignore: true)
+  _$$TermsOfUseStateImplCopyWith<_$TermsOfUseStateImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
